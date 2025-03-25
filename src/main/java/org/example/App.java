25aa0641170751329b8ec9b9f2f0370ab19ee019
melikeaytac <48melikeaytac@gmail.com>
2025-03-25 16:08:11 +0300

    week-5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,46 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-    }
+
+        HogwartsStudent harry = new HogwartsStudent("Harry Potter", "Gryffindor", 3);
+        HogwartsStudent draco = new HogwartsStudent("Draco Malfoy", "Slytherin", 3);
+        HogwartsStudent hermione = new HogwartsStudent("Hermione Granger", "Gryffindor", 3);
+
+
+        harry.learnSpell("Expelliarmus");
+        draco.learnSpell("Sectumsempra");
+        hermione.learnSpell("Alohomora");
+
+
+        System.out.println("Checking if Harry knows the a spell");
+        System.out.println("Does Harry know Expelliarmus? " + harry.knowsSpell("Expelliarmus"));
+
+
+        Spellbook spellbook = new Spellbook();
+        spellbook.addSpell("Expelliarmus");
+        spellbook.addSpell("Sectumsempra");
+        spellbook.addSpell("Alohomora");
+        System.out.println(" ");
+
+        System.out.println("Filtering a spell by prefix");
+        System.out.println("Spells starting with 'Ex': " + spellbook.getSpellsByPrefix("Ex"));
+
+        MagicClassroom classroom = new MagicClassroom();
+        classroom.addStudent(harry);
+        classroom.addStudent(draco);
+        classroom.addStudent(hermione);
+
+
+        System.out.println("Finding student by spell 'Sectumsempra': " + classroom.findStudentBySpell("Sectumsempra").getName());
+        System.out.println(" ");
+
+        System.out.println("Doing an exam");
+        WizardExam exam = new WizardExam(Arrays.asList("Expelliarmus", "Alohomora"));
+        exam.evaluate(harry);
+        exam.evaluate(hermione);
+        System.out.println(" ");
+
+        System.out.println("Making duels");
+        Duel.start(harry, draco, "Expelliarmus");
+        Duel.start(harry, draco, "Sectumsempra");    }
 }
